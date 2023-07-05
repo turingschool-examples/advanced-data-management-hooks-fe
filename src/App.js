656f84a6b0c96477a0e2843d49e528080b1dc5ea
NikSeif <nik.safe@gmail.com>
2023-07-05 08:05:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import  { useState } from 'react';
+import Ideas from './Ideas';
+
+import Form from './Form.js'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const dummyIdeas = [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+    ]
+  const [ideas, setIdeas] = useState(dummyIdeas)
+
+  function addIdea (newIdea) {
+    setIdeas([...ideas, newIdea])
+  }
+
+  function deleteIdea(id){
+    const filteredIdea = ideas.filter(idea => idea.id !== id)
+    setIdeas(filteredIdea)
+  }
+  return(
+    <main className='App'>
+        <h1>IdeaBox</h1>
+        <Form addIdea={addIdea}/>
+        <Ideas ideas={ideas} deleteIdea={deleteIdea}/>
+    </main>
+  )
 }
 
 export default App;
