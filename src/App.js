53d7721680ec2c0d06e25f416e84a00d51299a08
Kayla Wood <kaylaewood@gmail.com>
2023-08-24 12:03:39 -0600

    Remove dummy data

--- src/App.js
+++ src/App.js
@@ -5,30 +5,8 @@ import Form from './Form'
 import './App.css';
 
 function App(){
-  const dummyIdeas = [
-        { id: 101, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-        { id: 102, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-        { id: 103, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
-    ]
-  const [ideas, setIdeas] = useState(dummyIdeas)
+  const [ideas, setIdeas] = useState([])
   const [error, setError] = useState('')
-
-  function addIdea(newIdea) {
-    fetch('http://localhost:3001/api/v1/ideas', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: newIdea.id,
-        title: newIdea.title,
-        description: newIdea.description
-      }), 
-    })
-    .then(response => response.json())
-    .then(data => setIdeas([...ideas, data]))
-    .catch(error => setError(error.message)) 
-  }
 
   function getIdeas() {
     fetch('http://localhost:3001/api/v1/ideas')
@@ -40,6 +18,20 @@ function App(){
   useEffect(() => {
     getIdeas();
   }, [])
+
+  function addIdea(newIdea) {
+    fetch('http://localhost:3001/api/v1/ideas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newIdea), 
+    })
+    .then(response => response.json())
+    .then(data => setIdeas([...ideas, data]))
+    .catch(error => setError(error.message)) 
+  }
+
 
   function deleteIdea(id){
     fetch(`http://localhost:3001/api/v1/ideas/${id}`, { method: 'DELETE' })
