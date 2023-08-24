d934c4b20e42114cf0cb0b9e8d8a48b44d019465
Kayla Wood <kaylaewood@gmail.com>
2023-08-24 08:20:58 -0600

    Complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
 import Ideas from './Ideas';
 import Form from './From'
 
@@ -6,15 +6,27 @@ import './App.css';
 
 function App(){
   const dummyIdeas = [
-        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+        { id: 101, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 102, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 103, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
     ]
   const [ideas, setIdeas] = useState(dummyIdeas)
+  const [error, setError] = useState('')
 
-  function addIdea (newIdea) {
+  function addIdea(newIdea) {
     setIdeas([...ideas, newIdea])
   }
+
+  function getIdeas() {
+    fetch('http://localhost:3001/api/v1/ideas')
+    .then(response => response.json())
+    .then(data => setIdeas([...ideas, ...data]))
+    .catch(error => setError(error.message))
+  }
+
+  useEffect(() => {
+    getIdeas();
+  }, [])
 
   function deleteIdea(id){
     const filteredIdea = ideas.filter(idea => idea.id !== id)
@@ -24,6 +36,7 @@ function App(){
     <main className='App'>
         <h1>IdeaBox</h1>
         <Form addIdea={addIdea}/>
+        {error && <h2>{error}</h2>}
         <Ideas ideas={ideas} deleteIdea={deleteIdea}/>
     </main>
   )
